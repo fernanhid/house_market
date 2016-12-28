36b66150b3c34eac53b1110bf5cdd6e1b8e22239
Fernando <fernanhid@gmail.com>
2016-12-28 14:57:19 -0500

    new file

--- heroku_market.py
+++ heroku_market.py
@@ -180,8 +180,7 @@ def call_api(address, town):
 #     return sndmap
 
 
-
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
